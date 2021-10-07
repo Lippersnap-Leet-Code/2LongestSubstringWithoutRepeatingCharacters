76bcc9325d6e38edb3085f82bd44eb64b40eb771
Sam <sam.gordon@valpo.edu>
2021-10-07 00:58:35 -0500

    Interesting idea but only 1/4 of test cases pass

--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,31 @@
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+
         StringBuilder used = new StringBuilder();
         int answer = 0;
+        int current_substring = 0;
 
         for (int i = 0; i < s.length(); i++)
         {
             Character c = s.charAt(i);
             if (!used.toString().contains(c.toString())) {
-                answer++;
+                current_substring++;
                 used.append(c);
-            } else {
-
+            } else if (current_substring > answer) {
+                answer = current_substring;
+                used.delete(0, used.length());
+                used.append(c);
+                current_substring = 1;
             }
         }
+
+        if (current_substring > answer) {
+            answer = current_substring;
+        }
+
         return answer;
     }
 }
--- src/Substring.java
+++ src/Substring.java
@@ -2,8 +2,12 @@ public class Substring {
     public static void main(String[] args){
         Solution solver = new Solution();
 
-        String case1 = "abcabcbb";
-
-        System.out.println(solver.lengthOfLongestSubstring(case1));
+        //System.out.println(solver.lengthOfLongestSubstring("abcabcbb"));
+        //System.out.println(solver.lengthOfLongestSubstring("bbbbb"));
+        //System.out.println(solver.lengthOfLongestSubstring("pwwkew"));
+        //System.out.println(solver.lengthOfLongestSubstring(""));
+        //System.out.println(solver.lengthOfLongestSubstring(" "));
+        //System.out.println(solver.lengthOfLongestSubstring("au"));
+        System.out.println(solver.lengthOfLongestSubstring("dvdf"));
     }
 }
